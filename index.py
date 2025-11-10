0c2966211e34f14cb9b0cee2c8e8983f2f653e6c
EstebanLinharesBarral <elinaresb24@fpcoruna.afundacion.org>
2025-11-10 08:35:11 +0100

    Ejercicio 1 bien hecho

--- index.py
+++ index.py
@@ -1,42 +1,41 @@
 import random
 
 #Ejercicio 1
-print("El programa pensará un número entre 1 y un máximo que tú elijas. Intenta adivinarlo con la menor cantidad de intentos posible.")
-bool = False
-
-#Selector de dificultad
-while not bool:
-    dificultad = input("Elige un nivel de dificultad: 1. Fácil / 2. Media / 3. Difícil (Escoja entre 1, 2 y 3)")
-    if dificultad == "1" or dificultad == "2" or dificultad == "3":
-        bool = True
-    else:
-        print("Debe elegir una de las opciones")
-
 #Crear numero random y prueba de función
 def crearRandom(num):
     if num == "1":
         rm = random.randint(1, 50)
     elif num == "2":
-        rm = random.randit(1, 100)
+        rm = random.randint(1, 100)
     elif num == "3":
         rm = random.randint(1, 500)
     return rm
 
 #Bucle de juego
-random = crearRandom(dificultad)
+bool = False
 acierto = False
 intentos = 0
-nuevoJuego = False
+nuevoJuego = True
 
-while not nuevoJuego:
+print("El programa pensará un número entre 1 y un máximo que tú elijas. Intenta adivinarlo con la menor cantidad de intentos posible.")
+while nuevoJuego:
+    #Selector de dificultad
+    while not bool:
+        dificultad = input("Elige un nivel de dificultad: 1. Fácil / 2. Media / 3. Difícil (Escoja entre 1, 2 y 3)")
+        if dificultad == "1" or dificultad == "2" or dificultad == "3":
+            bool = True
+        else:
+            print("Debe elegir una de las opciones")
+    rnd = crearRandom(dificultad)
+
     while not acierto:
         try:
             intentos = intentos+1
             userNum = input("Intente adivinar el número, introduzca uno:")
             userNum = int(userNum)
-            if userNum > random:
+            if userNum > rnd:
                 print("Demasiado alto")
-            elif userNum < random:
+            elif userNum < rnd:
                 print("Demasiado bajo")
             else:
                 acierto = True
@@ -46,9 +45,12 @@ while not nuevoJuego:
     print(f"¡Acertaste! Nº de intentos: {intentos}")
     
     siNo = ""
-    while siNo.lower() != "s" or siNo.lower() != "n":
+    while siNo != "s" and siNo != "n":
         siNo = input("¿Quieres jugar de nuevo (s/n)")
-        if siNo.lower() == "s":
+        print(siNo)
+        if siNo == "s":
             nuevoJuego = True
-        elif siNo.lower() == "n":
+            acierto = False
+            bool = False
+        elif siNo == "n":
             nuevoJuego = False
